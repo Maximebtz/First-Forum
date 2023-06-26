87e0cfa9d784b01903e20abd8078f2482719bbf6
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-06-26 18:51:56 +0200

    Update

---
+++ view/layout.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <title></title>
+</head>
+<body>
+    <header>
+        <button class="login-btn" href="#">Log In</button>
+    </header>
+    <nav>
+        <div class="upper-nav-content">
+            <div class="title">
+                <img src="../public/img/Expedition Backpack.png" alt="backpacker icon">
+                <h1>TBF·Travel</h1>
+            </div>
+            <div class="links">
+                <div class="link">
+                    <img src="../public/img/Home.png" alt="home icon">
+                    <a id="home" href="#">Home</a>
+                </div>
+                <div class="link">
+                    <img src="../public/img/Globe.png" alt="globe icon">
+                    <a id="destinations" href="#">Destinations</a>
+                </div>
+                <div class="link">
+                    <img src="../public/img/Communication.png" alt="communication icon">
+                    <a id="forum" href="#">Forum</a>
+                </div>
+                <div class="link">
+                    <img src="../public/img/Brainstorm Skill.png" alt="inspiration icon">
+                    <a id="inspiration" href="#">Inspirations</a>
+                    
+                </div>
+                <div class="link">
+                    <img src="../public/img/Crystal Ball.png" alt="tips icon">
+                    <a id="tips" href="#">Tips</a>
+                </div>
+            </div>
+            <div class="contacts">
+                <h3>Contacts</h3>
+            </div>
+        </div>
+        <div class="darkmode" >
+            <div class="dark-mode-icon">
+                <img class="moon-symbol" src="../public/img/Moon-Symbol.png" />
+                <img class="sun" src="../public/img/sun.png" />
+            </div>
+            <div class="dark-mode-switch-bar">
+                <div class="rectangle-7" >
+                    <div class="ellipse-12" >
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="section-1">
+            <div class="title-background">
+                
+            </div>
+        </div>
+        
+        <div class="section-2">
+            <div class="wrapper-article">
+                <div class="article" >
+                    <figure>
+                        <img class="article-img" src="../public/img/pexels-roman-odintsov-4553618.jpg" />
+                    </figure>
+                    <div class="article-content">
+                        <h3 class="article-title" >
+                            La Philosophie du voyageur
+                        </h3>
+                        <article class="article-p" >
+                            Le voyage est une exploration de l&#039;âme,
+                             une quête philosophique qui transcende les frontières.
+                             Le voyageur cherche la vérité à travers les rencontres, les cultures et les paysages,
+                              son sac à dos étant le symbole de sa quête de liberté et de connaissance.
+                        </article>
+                    </div>
+                </div>
+                    
+                <div class="article article-2" >
+                    <div class="article-content">
+                        <h3 class="article-title" >
+                            Pourquoi Voyager ?
+                        </h3>
+                        <article class="article-p" >
+                            Le voyage, quête philosophique transcendant les frontières, 
+                            explore l&#039;âme du voyageur. À travers rencontres, cultures et paysages,
+                             il cherche la vérité, portant son sac à dos comme symbole de liberté et de quête de connaissance.
+                        </article>
+                    </div>
+                    <figure>
+                        <img class="article-img" src="../public/img/pexels-beth-easton-2433985.jpg" /> 
+                    </figure>
+                </div>
+                
+                <div class="article">
+                    <figure>
+                        <img class="article-img" src="../public/img/pexels-cottonbro-studio-10415671.jpg" />
+                    </figure>
+                    <div class="article-content">
+                        <h3 class="article-title" >
+                            Warning !!
+                        </h3>
+                        <article class="article-p" >
+                            Voyager en sécurité : se renseigner sur les destinations, 
+                            éviter les zones à risques et suivre les recommandations locales. 
+                            Protéger ses effets personnels, garder une copie des documents importants et 
+                            connaître les numéros d&#039;urgence locaux. Restez vigilant, respectez les coutumes 
+                            et profitez du voyage en minimisant les risques.
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer>
+            
+        </footer>
+    </body>
+    </html>
