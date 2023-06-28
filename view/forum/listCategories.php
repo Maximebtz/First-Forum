dc18313ee1286ede12ff04dbaff1fa9e259cfee4
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-06-28 20:07:40 +0200

    Update

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -4,22 +4,24 @@ $categories = $result["data"]['categories'];
     
 ?>
 <div class="section-2">
-    <div class="wrapper-list-topic">
-    <h2>Destinations</h2>
-        <div class="cards">
-        <?php
-            foreach($categories as $category ){
-        ?>
-                <a href="index.php?ctrl=forum&action=listTopics&id=">
-                    <div class='card'>
-                        <h3><?= $category->getName() ?> :</h3>
-                        <p><?= $category->getDescription() ?></p>
-                    </div>
-                </a>
-        <?php
-            }
-        ?>
-        </div>
+    <div class="wrapper-list-categories">
+            <button>Créer un nouveau forum</button>
+            <div class="cards">
+            <h2>Destinations</h2>
+            
+            <?php
+                foreach($categories as $category ){
+            ?>
+                    <a href="index.php?ctrl=forum&action=listTopics&id=">
+                        <div class='card'>
+                            <h3><?= $category->getName() ?> :</h3>
+                            <p><?= $category->getDescription() ?></p>
+                        </div>
+                    </a>
+            <?php
+                }
+            ?>
+            </div>
     </div>
    
 
